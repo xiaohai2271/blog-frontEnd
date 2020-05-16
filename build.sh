3e5524397b6ed1ff62e827539e1713a2e835375a
小海 <zh56462271@qq.com>
2020-05-16 22:25:13 +0800

    调整ci

--- build.sh
+++ build.sh
@@ -27,14 +27,14 @@ fi
 
 # index
 echo -e "\t\tBuild for index page "
-cd ./index && npm install && ng build --prod
+npm install && ng build --prod
 
 cd ./dist/index/ && tar -cf index.tar ./* && cp index.tar $basePath
 
-cd "$basePath"
+#cd "$basePath"
 
-# admin
-echo -e "\t\tBuild for admin page "
-cd $basePath/admin && npm install && ng build --prod
-cd ./dist/admin/ && sed '6s/\"\/\"/\"\/admin\/\"/g' index.html > index.txt && cp index.txt index.html
- cd .. && tar -cf admin.tar ./admin/ && cp admin.tar $basePath
+## admin
+#echo -e "\t\tBuild for admin page "
+#cd $basePath/admin && npm install && ng build --prod
+#cd ./dist/admin/ && sed '6s/\"\/\"/\"\/admin\/\"/g' index.html > index.txt && cp index.txt index.html
+# cd .. && tar -cf admin.tar ./admin/ && cp admin.tar $basePath
