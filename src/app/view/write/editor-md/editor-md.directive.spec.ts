b63697f717d6bf01e581455672da8b65ac28c71e
禾几海 <zh56462271@qq.com>
2020-10-10 00:24:46 +0800

    refactor: 删除残留代码

--- src/app/view/write/editor-md/editor-md.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EditorMdDirective } from './editor-md.directive';
-
-describe('EditorMdDirective', () => {
-  it('should create an instance', () => {
-    const directive = new EditorMdDirective();
-    expect(directive).toBeTruthy();
-  });
-});
